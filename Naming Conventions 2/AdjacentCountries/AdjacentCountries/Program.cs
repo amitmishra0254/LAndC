70fd75d3cde58a9e65401ee5a7c2a71ca6f1c3c3
amit.mishra <amit.mishra@ITT-AMIT-MI>
2024-12-18 14:38:22 +0530

    Added one more console project for getting AdjacentCountries.

---
+++ Naming Conventions 2/AdjacentCountries/AdjacentCountries/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace AdjacentCountries
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter a country code to get Adjacent Countries: ");
+                string countryCode = Console.ReadLine().Trim().ToUpper();
+                var countries = deserializeCountries();
+                if (!isValidCountry(countryCode))
+                {
+                    Console.WriteLine("Invalid country code. Please give a country code from the belows.");
+                    Console.WriteLine(string.Join(", ", countries.Keys.ToList()).ToString());
+                    return;
+                }
+                Console.WriteLine(string.Join(", ", countries[countryCode]));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong. Please try again later.");
+            }
+        }
+
+        public static Dictionary<string, List<string>> deserializeCountries()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string countryDirectory = Directory.GetParent(currentDirectory).Parent.Parent.FullName + "//Country.json";
+            string json = File.ReadAllText(countryDirectory);
+            var countryDictionary = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(json);
+            return countryDictionary;
+        }
+
+        public static bool isValidCountry(string countryCode)
+        {
+            var countryDictionary = deserializeCountries();
+            return countryDictionary.Keys.Contains(countryCode);
+        }
+    }
+}
