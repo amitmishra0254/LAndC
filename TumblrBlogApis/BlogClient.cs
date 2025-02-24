46e48e54a1d41fa8a55a58128dee2d90297dceca
amit.mishra <amit.mishra@ITT-AMIT-MI>
2025-02-24 19:27:14 +0530

    Completed the week 3 assignment.

---
+++ TumblrBlogApis/BlogClient.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TumblrBlogApis
+{
+    public interface BlogClient
+    {
+        Task<JObject> fetchBlog(string blogName, string parameters = "?num={1}");
+    }
+}
