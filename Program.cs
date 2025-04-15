7735e55389a4e68fc246559a8f1c100300689d79
amit.mishra <amit.mishra@ITT-AMIT-MI>
2025-04-15 21:09:12 +0530

    Refactored the code for Customer and Wallet class and added the explaination for Employee class for being an Object.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Customer deliveryBoy = new Customer("John", "Doe");
+        float payment = 2.0f;
+
+        if (deliveryBoy.getWalletBalance() >= payment)
+        {
+            deliveryBoy.withdrawMoney(payment);
+            Console.WriteLine($"Payment of {payment} was successful. Remaining balance: {deliveryBoy.GetWalletBalance()}");
+        }
+        else
+        {
+            Console.WriteLine("Not enough balance, come back later and get my money.");
+        }
+    }
+}
