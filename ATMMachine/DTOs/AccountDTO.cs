4f9e52a5da9096c0908f6c03ea4da6626bf4904d
amit.mishra <amit.mishra@ITT-AMIT-MI>
2025-04-22 20:48:56 +0530

    Segregated classes in different files.

---
+++ ATMMachine/DTOs/AccountDTO.cs
@@ -0,0 +1,10 @@
+﻿using ATMMachine.Enums;
+
+namespace ATMMachine.DTOs
+{
+    public class AccountDTO
+    {
+        public int Pin { get; set; }
+        public AccountType AccountType { get; set; }
+    }
+}
