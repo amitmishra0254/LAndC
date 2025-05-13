5e72edcd9a53a444ea005fdf9a1dff697b8b629c
amit.mishra <amit.mishra@ITT-AMIT-MI>
2025-05-13 18:47:35 +0530

    Completed the Assignment 8: Unit Tests

---
+++ DivisorCounter/DivisorCounter.cs
@@ -0,0 +1,30 @@
+﻿public class DivisorCounter
+{
+    public static int CountMatchingDivisors(int number)
+    {
+        if (number < 0) throw new ArgumentException("Input must be non-negative");
+
+        int count = 0;
+        for (int index = 1; index < number; index++)
+        {
+            if (CountDivisors(index) == CountDivisors(index + 1))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int CountDivisors(int num)
+    {
+        int count = 0;
+        for (int index = 1; index <= Math.Sqrt(num); index++)
+        {
+            if (num % index == 0)
+            {
+                count += (index == num / index) ? 1 : 2;
+            }
+        }
+        return count;
+    }
+}
